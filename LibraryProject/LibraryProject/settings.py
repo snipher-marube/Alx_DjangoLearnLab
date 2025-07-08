b49be300631961aa4e14f3bde8e4b01003e8c791
Snipher Marube <sniphermarube@gmail.com>
2025-07-08 16:05:37 +0300

    new

--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gke+(b4)pk#vl$4exq795x&9c!(+qael3g^sr9y3m=!+c!+o@d'
+SECRET_KEY = 'django-insecure-%tc_a4#*w=uzwsv%l9f$99%6e@izfi5ev%t*#6dz+zsi^e3^(2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ LibraryProject/README.md
@@ -0,0 +1,6 @@
+### Library Project
+This project is a library that provides various functionalities for data processing and analysis. It includes modules for data manipulation, statistical analysis, and visualization.
+### Features
+- Data manipulation functions
+- Statistical analysis tools
+- Visualization utilities
